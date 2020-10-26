98e9f3d98e75ccdc8b96bf05eaa636e988485a08
tao.zhang <scorttee1437.se@gmail.com>
2020-10-26 21:24:53 +0800

    清除无用代码

--- QtWidgetsApplication1/QtWidgetsApplication1.cpp
+++
@@ -1,9 +0,0 @@
-#include "QtWidgetsApplication1.h"
-#include <QRegExpValidator>
-QtWidgetsApplication1::QtWidgetsApplication1(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui->setupUi(this);
-    QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
-    ui->lineEdit->setValidator(new QRegExpValidator(regExp, this));
-}
